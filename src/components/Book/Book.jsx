225852f0ced7145df1619f95a0456d4bf5117dbf
datta-toma <tomadatta15@gmail.com>
2024-03-26 00:30:36 +0600

    error page create

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,6 +1,6 @@
 import { FaRegStar } from "react-icons/fa";
 const Book = ({book}) => {
-    const {id, bookName, image, review, category, rating, tags} = book;
+    const {id, bookName, image, category, rating, tags, author} = book;
     return (
         <div>
            <div className="card w-96 bg-base-100 shadow-xl mt-10">
@@ -8,7 +8,7 @@ const Book = ({book}) => {
                 <div className="card-body">
                     <p className="text-green-400 font-bold">{tags.join(" ")}</p>
                     <h2 className="card-title">{bookName}</h2>
-                    <p>{review}</p><hr></hr>
+                    <p>Author: {author}</p><hr></hr>
                     <div className="flex">
                     <p>{category}</p>
                    <p className="flex gap-2"><FaRegStar></FaRegStar>{rating}</p>
--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2> oop!!!</h2>
-            <Link to="/">Go Back</Link>
+        <div className="text-center mt-72 space-y-6">
+            <h2 className="text-9xl font-bold"> 404</h2>
+            <p className="text-4xl">Not Found Page</p>
+            <Link to="/"><button className="btn btn-active mt-14 ">Go Back</button></Link>
         </div>
     );
 };
